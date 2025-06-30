00b06c608e52975bb1b7011821845574f82e9157
adityamandal-Developer <mandaladitya901@gmail.com>
2025-06-30 09:10:59 +0530

    fixed inngest error

--- t3demo/src/inngest/functions.ts
+++ t3demo/src/inngest/functions.ts
@@ -55,13 +55,15 @@ export const processVideo = inngest.createFunction(
           });
         });
 
-        await step.fetch(env.PROCESS_VIDEO_ENDPOINT, {
-          method: "POST",
-          body: JSON.stringify({ s3_key: s3Key }),
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${env.PROCESS_VIDEO_ENDPOINT_AUTH}`,
-          },
+        await step.run("call-modal-endpoint", async () => {
+          await fetch(env.PROCESS_VIDEO_ENDPOINT.trim(), {
+            method: "POST",
+            body: JSON.stringify({ s3_key: s3Key }),
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${env.PROCESS_VIDEO_ENDPOINT_AUTH}`,
+            },
+          });
         });
 
         const { clipsFound } = await step.run(
