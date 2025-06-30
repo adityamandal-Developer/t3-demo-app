062b9828cd42f7794df5a5880e9e5323fb3f81db
adityamandal-Developer <mandaladitya901@gmail.com>
2025-06-30 06:36:12 +0530

    first commit

---
+++ t3demo/src/components/form-login.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "./ui/button";
+import { cn } from "~/lib/utils";
+
+const FormLogin = () => {
+  return (
+    <div className={cn("text-2xl")}>
+      <Button>testtt</Button>
+    </div>
+  );
+};
+
+export default FormLogin;
